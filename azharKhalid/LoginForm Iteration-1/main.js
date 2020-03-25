bdd3fd3ef9ee7b420efb13e0a2ae78c4d9c13cb0
azharKhalid <azhar.khalid10@gmail.com>
2020-03-25 13:02:00 +0530

    sucess page added

--- azharKhalid/LoginForm Iteration-1/main.js
+++ azharKhalid/LoginForm Iteration-1/main.js
@@ -2,41 +2,66 @@
 
 document.getElementById("submit").addEventListener("click", event => {
   event.preventDefault();
+  //Capturing all the input value entered by user
   let email = document.getElementById("email").value;
   let mobileNo = document.getElementById("mobileNo").value;
   let password1 = document.getElementById("password1").value;
   let password2 = document.getElementById("password2").value;
-  console.log("Submitted");
+  //Function for validating email
   validateEmail(email);
+  //Function for validating mobile Number
   validateMobileNumber(mobileNo);
+  //Function for validating Password
   validatePassword(password1, password2);
+  //Condition for redirecting the page after successfull entering of all the inputs
+  if (
+    validateEmail(email) &&
+    validateMobileNumber(mobileNo) &&
+    validatePassword(password1, password2)
+  ) {
+    window.location.href = "successPage.html";
+  } else {
+    return;
+  }
 });
 
+//Function for validating mobile Number
 function validateMobileNumber(mobileNo) {
   if (mobileNo.length < 10) {
     let mobileSmall = document.getElementById("mobileSmall");
     mobileSmall.style.display = "block";
+    return false;
   } else {
-    return console.log(mobileNo);
+    return true;
   }
 }
 
+//Function for validating Password
 function validatePassword(password1, password2) {
-  if (password1 !== password2) {
-    let passwordSmall = document.getElementById("passwordSmall");
+  if (password1 == "" || password2 == "") {
+    let passwordSmall = document.getElementById("passwordSmall2");
     passwordSmall.style.display = "block";
+    return false;
   } else {
-    return console.log(password1);
+    if (password1 !== password2) {
+      let passwordSmall = document.getElementById("passwordSmall1");
+      passwordSmall.style.display = "block";
+      return false;
+    } else {
+      return true;
+    }
   }
 }
 
+//Function for validating email
 function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   let inputEmail = re.test(String(email).toLowerCase());
   if (inputEmail) {
-    return console.log(email);
+    return true;
   } else {
     let emailSmall = document.getElementById("emailSmall");
     emailSmall.style.display = "block";
+    return false;
   }
 }
